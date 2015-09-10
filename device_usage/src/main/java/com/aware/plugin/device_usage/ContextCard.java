d1a9bef2a6c4d2fc5f5c41d984e34917054370b3
denzilferreira <denzil.ferreira@ee.oulu.fi>
2015-09-10 22:18:18 +0300

    MPAndroidChart

--- device_usage/src/main/java/com/aware/plugin/device_usage/ContextCard.java
+++ device_usage/src/main/java/com/aware/plugin/device_usage/ContextCard.java
@@ -11,30 +11,30 @@ import android.widget.TextView;
 import com.aware.plugin.device_usage.Provider.DeviceUsage_Data;
 import com.aware.utils.Converters;
 import com.aware.utils.IContextCard;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 
-import org.achartengine.ChartFactory;
-import org.achartengine.GraphicalView;
-import org.achartengine.chart.BarChart.Type;
-import org.achartengine.chart.PointStyle;
-import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
-import org.achartengine.renderer.XYMultipleSeriesRenderer;
-import org.achartengine.renderer.XYSeriesRenderer;
-
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class ContextCard implements IContextCard {
 
+	private static BarChart mChart;
+
     /**
      * Empty constructor required for Java reflection to load the card
      */
-    public ContextCard(){};
+    public ContextCard(){}
 
-	static String[] x_hours = new String[]{"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"};
-	
 	public View getContextCard( Context context ) {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View card = inflater.inflate(R.layout.layout, null);
+        LinearLayout chart = (LinearLayout) card.findViewById(R.id.chart_container);
+        mChart = (BarChart) chart.findViewById(R.id.usage_chart);
 
         //Get today's time from the beginning in milliseconds
         Calendar c = Calendar.getInstance();
@@ -61,74 +61,66 @@ public class ContextCard implements IContextCard {
             just_off.setText( Converters.readable_elapsed( (long) phone_off ) );
         }
         if( off != null && ! off.isClosed() ) off.close();
-        
-        LinearLayout chart = (LinearLayout) card.findViewById(R.id.chart_usage); 
-        chart.removeAllViews();
 
-		//stores screen on counts grouped per hour
-		int[] frequencies = new int[24];
-		
+		chart.removeAllViews();
+		chart.addView(drawGraph(context));
+		chart.invalidate();
+
+        return card;
+	}
+
+	private BarChart drawGraph( Context context ) {
+
+		//Get today's time from the beginning in milliseconds
+		Calendar c = Calendar.getInstance();
+		c.setTimeInMillis(System.currentTimeMillis());
+		c.set(Calendar.HOUR_OF_DAY, 0);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		c.set(Calendar.MILLISECOND, 0);
+
+		ArrayList<String> x_hours = new ArrayList<>();
+		for(int i=0; i<24; i++) {
+			x_hours.add(String.valueOf(i));
+		}
+
+		ArrayList<BarEntry> barEntries = new ArrayList<>();
 		//add frequencies to the right hour buffer
-		Cursor off_times = context.getContentResolver().query(DeviceUsage_Data.CONTENT_URI, new String[]{ "count(*) as frequency","strftime('%H',"+ DeviceUsage_Data.TIMESTAMP + "/1000, 'unixepoch', 'localtime')+0 as time_of_day" }, DeviceUsage_Data.ELAPSED_DEVICE_ON + " > 0 AND " + DeviceUsage_Data.TIMESTAMP + " >= " + c.getTimeInMillis() + " ) GROUP BY ( time_of_day ", null, DeviceUsage_Data.TIMESTAMP + " ASC");
+		Cursor off_times = context.getContentResolver().query(DeviceUsage_Data.CONTENT_URI, new String[]{ "count(*) as frequency","strftime('%H',"+ DeviceUsage_Data.TIMESTAMP + "/1000, 'unixepoch', 'localtime')+0 as time_of_day" }, DeviceUsage_Data.ELAPSED_DEVICE_ON + " > 0 AND " + DeviceUsage_Data.TIMESTAMP + " >= " + c.getTimeInMillis() + " ) GROUP BY ( time_of_day ", null, "time_of_day ASC");
 		if( off_times != null && off_times.moveToFirst() ) {
 			do{
-				frequencies[off_times.getInt(1)] = off_times.getInt(0);
+				barEntries.add( new BarEntry(off_times.getInt(0), off_times.getInt(1)) );
 			} while( off_times.moveToNext() );
 		}
-        if( off_times != null && ! off_times.isClosed()) off_times.close();
-		
-		XYSeries xy_series = new XYSeries("Device activity");
-		for( int i = 0; i<frequencies.length; i++ ) {
-			xy_series.add(i, frequencies[i]);
-		}
-		
-		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-		dataset.addSeries(xy_series);
-		
-		//Setup the line colors, labels, etc
-		XYSeriesRenderer series_renderer = new XYSeriesRenderer();
-		series_renderer.setColor(Color.parseColor("#33B5E5"));
-		series_renderer.setPointStyle(PointStyle.POINT);
-		series_renderer.setDisplayChartValues(false);
-		series_renderer.setLineWidth(2);
-		series_renderer.setFillPoints(false);
-		
-		//Setup graph colors, labels, etc
-		XYMultipleSeriesRenderer dataset_renderer = new XYMultipleSeriesRenderer();
-		dataset_renderer.setChartTitle("Today's device activity");
-		dataset_renderer.setLabelsColor(Color.BLACK);
-		dataset_renderer.setDisplayValues(true);
-		dataset_renderer.setFitLegend(false);
-		dataset_renderer.setXLabelsColor(Color.BLACK);
-		dataset_renderer.setYLabelsColor(0, Color.BLACK);
-		dataset_renderer.setLegendHeight(0);
-		dataset_renderer.setYLabels(0);
-        dataset_renderer.setYTitle("Frequency");
-        dataset_renderer.setXTitle("Time of day");
-		dataset_renderer.setZoomButtonsVisible(false);
-		dataset_renderer.setXLabels(0);
-		dataset_renderer.setPanEnabled(false);
-		dataset_renderer.setShowGridY(false);
-		dataset_renderer.setClickEnabled(false);
-		dataset_renderer.setAntialiasing(true);
-		dataset_renderer.setAxesColor(Color.BLACK);
-		dataset_renderer.setApplyBackgroundColor(true);
-		dataset_renderer.setBackgroundColor(Color.WHITE);
-		dataset_renderer.setMarginsColor(Color.WHITE);
-		
-		for(int i=0; i< x_hours.length; i++) {
-			dataset_renderer.addXTextLabel(i, x_hours[i]);
-		}
-		
-		//Add the series renderer to the chart renderer
-		dataset_renderer.addSeriesRenderer(series_renderer);
-		
-		//Create the chart with our data and setup
-		GraphicalView mChart = ChartFactory.getBarChartView(context, dataset, dataset_renderer, Type.DEFAULT); //show bar chart
-		//mChart = (GraphicalView) ChartFactory.getLineChartView(context, dataset, dataset_renderer); //show line chart
-        
-        chart.addView(mChart);
-        
-        return card;
+		if( off_times != null && ! off_times.isClosed()) off_times.close();
+
+		BarDataSet dataSet = new BarDataSet(barEntries, "Amount of times used");
+		dataSet.setColor(Color.parseColor("#33B5E5"));
+
+		BarData data = new BarData(x_hours, dataSet);
+		mChart.setMinimumHeight(200);
+		mChart.setBackgroundColor(Color.WHITE);
+		mChart.setDrawGridBackground(false);
+		mChart.setDrawBorders(false);
+
+		YAxis left = mChart.getAxisLeft();
+		left.setDrawLabels(false);
+		left.setDrawGridLines(false);
+		left.setDrawAxisLine(false);
+		YAxis right = mChart.getAxisRight();
+		right.setDrawAxisLine(false);
+		right.setDrawLabels(false);
+		right.setDrawGridLines(false);
+
+		XAxis bottom = mChart.getXAxis();
+		bottom.setPosition(XAxis.XAxisPosition.BOTTOM);
+		bottom.setSpaceBetweenLabels(0);
+		bottom.setDrawGridLines(false);
+
+		mChart.setData(data);
+		mChart.animateX(1000);
+		mChart.invalidate();
+
+		return mChart;
 	}
 }
