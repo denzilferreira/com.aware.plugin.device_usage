a94cfca94f8217c86ca1105a025c969d5486bc7a
Denzil Ferreira <ferreiradenzil@gmail.com>
2016-12-02 10:42:57 +0200

    - Make plugin also available as a library for self-contained apps.

--- com.aware.plugin.device_usage.lib/src/main/java/com/aware/plugin/device_usage/ContextCard.java
+++ com.aware.plugin.device_usage.lib/src/main/java/com/aware/plugin/device_usage/ContextCard.java
@@ -1,4 +1,4 @@
-package com.aware.plugin.device_usage;
+package com.aware.plugin.device_usage.lib;
 
 import android.content.Context;
 import android.database.Cursor;
@@ -26,9 +26,9 @@ public class ContextCard implements IContextCard {
 
 	public View getContextCard( Context context ) {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View card = inflater.inflate(com.aware.plugin.device_usage.lib.R.layout.layout, null);
+		View card = inflater.inflate(R.layout.layout, null);
 
-        BarChart chart = (BarChart) card.findViewById(com.aware.plugin.device_usage.lib.R.id.barPlot);
+        BarChart chart = (BarChart) card.findViewById(R.id.barPlot);
 
         //Get today's time from the beginning in milliseconds
         Calendar c = Calendar.getInstance();
@@ -38,8 +38,8 @@ public class ContextCard implements IContextCard {
         c.set(Calendar.SECOND, 0);
         c.set(Calendar.MILLISECOND, 0);
 
-		TextView just_off = (TextView) card.findViewById(com.aware.plugin.device_usage.lib.R.id.device_off);
-        TextView average_off = (TextView) card.findViewById(com.aware.plugin.device_usage.lib.R.id.average_unused);
+		TextView just_off = (TextView) card.findViewById(R.id.device_off);
+        TextView average_off = (TextView) card.findViewById(R.id.average_unused);
 
         String[] columns = new String[]{"AVG(" + Provider.DeviceUsage_Data.ELAPSED_DEVICE_OFF + ") as average"};
         Cursor avg_off = context.getContentResolver().query(Provider.DeviceUsage_Data.CONTENT_URI, columns, Provider.DeviceUsage_Data.ELAPSED_DEVICE_OFF + " > 0", null, null);
--- com.aware.plugin.device_usage.lib/src/main/java/com/aware/plugin/device_usage/Settings.java
+++ com.aware.plugin.device_usage.lib/src/main/java/com/aware/plugin/device_usage/Settings.java
@@ -1,4 +1,4 @@
-package com.aware.plugin.device_usage;
+package com.aware.plugin.device_usage.lib;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -22,7 +22,7 @@ public class Settings extends PreferenceActivity implements OnSharedPreferenceCh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(com.aware.plugin.device_usage.lib.R.xml.preferences);
+        addPreferencesFromResource(R.xml.preferences);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
     }
