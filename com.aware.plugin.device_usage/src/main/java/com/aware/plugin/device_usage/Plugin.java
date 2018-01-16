e3b5791937a24dfb1db82b369db30d4a19492032
denzilferreira <ferreiradenzil@gmail.com>
2018-01-16 14:59:58 +0200

    - Respect periodic sync.

--- com.aware.plugin.device_usage/src/main/java/com/aware/plugin/device_usage/Plugin.java
+++ com.aware.plugin.device_usage/src/main/java/com/aware/plugin/device_usage/Plugin.java
@@ -115,7 +115,7 @@ public class Plugin extends Aware_Plugin {
 
             if (!Aware.isSyncEnabled(this, Provider.getAuthority(this)) && Aware.isStudy(this) && getApplicationContext().getPackageName().equalsIgnoreCase("com.aware.phone") || getApplicationContext().getResources().getBoolean(R.bool.standalone)) {
                 ContentResolver.setIsSyncable(Aware.getAWAREAccount(this), Provider.getAuthority(this), 1);
-                ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Provider.getAuthority(this), true);
+                //ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Provider.getAuthority(this), true);
                 ContentResolver.addPeriodicSync(
                         Aware.getAWAREAccount(this),
                         Provider.getAuthority(this),
@@ -137,7 +137,7 @@ public class Plugin extends Aware_Plugin {
         Aware.stopScreen(this);
 
         if (Aware.isStudy(this) && (getApplicationContext().getPackageName().equalsIgnoreCase("com.aware.phone") || getApplicationContext().getResources().getBoolean(R.bool.standalone))) {
-            ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Provider.getAuthority(this), false);
+            //ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Provider.getAuthority(this), false);
             ContentResolver.removePeriodicSync(
                     Aware.getAWAREAccount(this),
                     Provider.getAuthority(this),
