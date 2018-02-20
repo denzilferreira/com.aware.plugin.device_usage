716499654d39f2712b8c9a738283d97d0172794d
denzilferreira <ferreiradenzil@gmail.com>
2018-02-20 14:43:08 +0200

    - Aware.isStudy() check for sync-adapter.

--- com.aware.plugin.device_usage/src/main/java/com/aware/plugin/device_usage/Plugin.java
+++ com.aware.plugin.device_usage/src/main/java/com/aware/plugin/device_usage/Plugin.java
@@ -115,7 +115,7 @@ public class Plugin extends Aware_Plugin {
                 }
             });
 
-            if (Aware.isStudy(this) && !Aware.isSyncEnabled(this, Provider.getAuthority(this))) {
+            if (Aware.isStudy(this)) {
                 Account aware_account = Aware.getAWAREAccount(getApplicationContext());
                 String authority = Provider.getAuthority(getApplicationContext());
                 long frequency = Long.parseLong(Aware.getSetting(this, Aware_Preferences.FREQUENCY_WEBSERVICE)) * 60;
@@ -123,7 +123,7 @@ public class Plugin extends Aware_Plugin {
                 ContentResolver.setIsSyncable(aware_account, authority, 1);
                 ContentResolver.setSyncAutomatically(aware_account, authority, true);
                 SyncRequest request = new SyncRequest.Builder()
-                        .syncPeriodic(frequency, frequency/3)
+                        .syncPeriodic(frequency, frequency / 3)
                         .setSyncAdapter(aware_account, authority)
                         .setExtras(new Bundle()).build();
                 ContentResolver.requestSync(request);
@@ -141,14 +141,12 @@ public class Plugin extends Aware_Plugin {
         Aware.setSetting(this, Settings.STATUS_PLUGIN_DEVICE_USAGE, false);
         Aware.stopScreen(this);
 
-        if (Aware.isSyncEnabled(this, Provider.getAuthority(this))) {
-            ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Provider.getAuthority(this), false);
-            ContentResolver.removePeriodicSync(
-                    Aware.getAWAREAccount(this),
-                    Provider.getAuthority(this),
-                    Bundle.EMPTY
-            );
-        }
+        ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Provider.getAuthority(this), false);
+        ContentResolver.removePeriodicSync(
+                Aware.getAWAREAccount(this),
+                Provider.getAuthority(this),
+                Bundle.EMPTY
+        );
 
         Aware.stopAWARE(this);
     }
