dce9b8cb5cd51e01f18ac765c3c3186f59d5d698
Denzil Ferreira <ferreiradenzil@gmail.com>
2019-06-12 12:24:10 +0300

    - Updated libraries, documentation in GitHub.

--- com.aware.plugin.device_usage/src/main/java/com/aware/plugin/device_usage/syncadapters/Device_Usage_Sync.java
+++ com.aware.plugin.device_usage/src/main/java/com/aware/plugin/device_usage/syncadapters/Device_Usage_Sync.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.IBinder;
 
-import androidx.annotation.Nullable;
 import com.aware.plugin.device_usage.Provider;
 import com.aware.syncadapters.AwareSyncAdapter;
 
@@ -33,7 +32,6 @@ public class Device_Usage_Sync extends Service {
         }
     }
 
-    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return sSyncAdapter.getSyncAdapterBinder();
